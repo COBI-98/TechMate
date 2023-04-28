102b401f3e056dc88adc8a1f89cb9b657fde3fe4
COBI-98 <tkdgus968@naver.com>
2023-04-29 02:01:21 +0900

    test: git hook pre-commit 임포트, 코드서식 체크

--- src/main/java/com/cobi/techmate/controller/TestController.java
+++ src/main/java/com/cobi/techmate/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.cobi.techmate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class TestController {
 
   @GetMapping("hello")
   public String hello(Model model) {
+
     model.addAttribute("data", "hello!!");
+
     return "hello";
   }
 }
