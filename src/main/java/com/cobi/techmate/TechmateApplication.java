b01d88df4f48f33298417985486033fbe3dd1bf2
COBI-98 <tkdgus968@naver.com>
2023-04-29 01:07:20 +0900

    feat: setup techmate webproject basic frame

---
+++ src/main/java/com/cobi/techmate/TechmateApplication.java
@@ -0,0 +1,12 @@
+package com.cobi.techmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmateApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TechmateApplication.class, args);
+	}
+
+}
